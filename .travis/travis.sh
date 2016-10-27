f2f071b4983243b11801fa477172989b82c2f0a3
Kei Okada <kei.okada@gmail.com>
2016-10-27 23:45:52 +0900

    Rebase from 'upstream'

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -2,13 +2,22 @@
 
 set -x
 
+export CI_SOURCE_PATH=$(pwd)
+export REPOSITORY_NAME=${PWD##*/}
+
+ANSI_RED="\033[31;1m"
+ANSI_GREEN="\033[32;1m"
+ANSI_BLUE="\033[34;1m"
+ANSI_RESET="\033[0m"
+ANSI_CLEAR="\033[0K"
+
 function travis_time_start {
     set +x
     TRAVIS_START_TIME=$(date +%s%N)
     TRAVIS_TIME_ID=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 8 | head -n 1)
     TRAVIS_FOLD_NAME=$1
-    echo -e "\e[0Ktraivs_fold:start:$TRAVIS_FOLD_NAME"
-    echo -e "\e[0Ktraivs_time:start:$TRAVIS_TIME_ID\e[34m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+    echo -e "${ANSI_CLEAR}traivs_fold:start:$TRAVIS_FOLD_NAME"
+    echo -e "${ANSI_CLEAR}traivs_time:start:$TRAVIS_TIME_ID${ANSI_BLUE}>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>${ANSI_RESET}"
     set -x
 }
 function travis_time_end {
@@ -16,14 +25,72 @@ function travis_time_end {
     _COLOR=${1:-32}
     TRAVIS_END_TIME=$(date +%s%N)
     TIME_ELAPSED_SECONDS=$(( ($TRAVIS_END_TIME - $TRAVIS_START_TIME)/1000000000 ))
-    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n\e[0K"
-    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME\e[${_COLOR}m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-    echo -e "\e[0K\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec\e[0m"
+    echo -e "traivs_time:end:$TRAVIS_TIME_ID:start=$TRAVIS_START_TIME,finish=$TRAVIS_END_TIME,duration=$(($TRAVIS_END_TIME - $TRAVIS_START_TIME))\n${ANSI_CLEAR}"
+    echo -e "traivs_fold:end:$TRAVIS_FOLD_NAME\e[${_COLOR}m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<${ANSI_RESET}"
+    echo -e "${ANSI_CLEAR}\e[${_COLOR}mFunction $TRAVIS_FOLD_NAME takes $(( $TIME_ELAPSED_SECONDS / 60 )) min $(( $TIME_ELAPSED_SECONDS % 60 )) sec${ANSI_RESET}"
     set -x
 }
 
+
+echo "Running jsk_travis/travis.sh whose version is $(cd $CI_SOURCE_PATH/.travis && git describe --all)."
+
+
+travis_time_start is_jsk_travis_upgraded
+
+# Check if jsk_travis is upgraded, because downgrading jsk_travis is not supported.
+if [ "$(git diff origin/master HEAD $CI_SOURCE_PATH/.travis)" != "" ] ; then
+  # HASH_TO_HASH represents: "jsk_travis version commited to origin/master" .. "jsk_travis to be commited with this PR"
+  # ex. 5f047fd5a8c0714c091b965b80b1f3719697c36a...0417ddc12c0b8ca4d10a86844745dd1279534845
+  HASH_TO_HASH=$(git diff origin/master HEAD $CI_SOURCE_PATH/.travis | grep .*Subproject | sed s'@.*Subproject commit @@' | sed 'N;s/\n/.../')
+  COMMITS=$(cd $CI_SOURCE_PATH/.travis/; git log --oneline --graph --left-right --first-parent --decorate $HASH_TO_HASH)
+  if [ $(echo "$COMMITS" | grep -c '^<' ) -eq 0 ]; then
+    echo INFO: jsk_travis is successfully upgraded comparing the version commited to origin/master.
+    echo It is $(echo "$COMMITS" | grep -c '^>') commits ahead.
+  else
+    echo ERROR: jsk_travis is downgraded comparing the version commited to origin/master, and this is not supported.
+    echo It is $(echo "$COMMITS" | grep -c '^<') commits behind, and the commits are below:
+    echo "$COMMITS" | grep -c '^<'
+    error
+  fi
+fi
+
+travis_time_end is_jsk_travis_upgraded
+
+
 # set default values to env variables
 [ "${USE_TRAVIS// }" = "" ] && USE_TRAVIS=false
+
+# Deprecated environmental variables
+[ ! -z $BUILDER ] && [ "$BUILDER" != catkin ] && ( echo "ERROR: $BUILDER is not supported. BUILDER env is deprecated and only 'catkin' is supported for the build."; exit 1; )
+[ ! -z $ROSWS ] && [ "$ROSWS" != wstool ] && ( echo "ERROR: $ROSWS is not supported. ROSWS env is deprecated and only 'wstool' is supported for workspace management."; exit 1; )
+
+# docker on travis
+if [ "$USE_DOCKER" = true ]; then
+  if [ "$DOCKER_IMAGE" = "" ]; then
+    case $ROS_DISTRO in
+      hydro) DISTRO=precise;;
+      indigo|jade) DISTRO=trusty;;
+      kinetic) DISTRO=xenial;;
+      *) DISTRO=trusty;;
+    esac
+    export DOCKER_IMAGE=ubuntu:$DISTRO
+  fi
+  docker run -it -v $HOME:$HOME \
+    -e TRAVIS_BRANCH -e TRAVIS_COMMIT -e TRAVIS_JOB_ID -e TRAVIS_OS_NAME -e TRAVIS_PULL_REQUEST -e TRAVIS_REPO_SLUG \
+    -e CI_SOURCE_PATH -e HOME -e REPOSITORY_NAME \
+    -e BUILD_PKGS -e TARGET_PKGS -e TEST_PKGS \
+    -e BEFORE_SCRIPT -e BUILDER -e EXTRA_DEBS -e USE_DEB \
+    -e ROS_DISTRO -e ROS_LOG_DIR -e ROS_REPOSITORY_PATH -e ROSWS \
+    -e CATKIN_TOOLS_BUILD_OPTIONS -e CATKIN_TOOLS_CONFIG_OPTIONS \
+    -e CATKIN_PARALLEL_JOBS -e CATKIN_PARALLEL_TEST_JOBS \
+    -e ROS_PARALLEL_JOBS -e ROS_PARALLEL_TEST_JOBS \
+    -e ROSDEP_ADDITIONAL_OPTIONS -e ROSDEP_UPDATE_QUIET \
+    -e SUDO_PIP -e USE_PYTHON_VIRTUALENV \
+    -e NOT_TEST_INSTALL \
+    -t $DOCKER_IMAGE bash -c 'cd $CI_SOURCE_PATH; .travis/docker.sh'
+  DOCKER_EXIT_CODE=$?
+  exit $DOCKER_EXIT_CODE
+fi
 
 if [ "$USE_TRAVIS" != "true" ] && [ "$ROS_DISTRO" == "indigo" -o "$ROS_DISTRO" == "jade" -o "$ROS_DISTRO" == "kinetic" -o "${USE_JENKINS}" == "true" ] && [ "$TRAVIS_JOB_ID" ]; then
     pip install --user python-jenkins -q
@@ -37,15 +104,7 @@ function error {
     exit 1
 }
 
-[ "$BUILDER" == rosbuild ] && ( echo "$BUILDER is no longer supported"; exit 1; )
-[ "$ROSWS" == rosws ] && ( echo "$ROSWS is no longer supported"; exit 1; )
-BUILDER=catkin
-ROSWS=wstool
-
 trap error ERR
-
-git branch --all
-if [ "`git diff origin/master FETCH_HEAD .travis`" != "" ] ; then DIFF=`git diff origin/master FETCH_HEAD .travis | grep .*Subproject | sed s'@.*Subproject commit @@' | sed 'N;s/\n/.../'`; (cd .travis/;git log --oneline --graph --left-right --first-parent --decorate $DIFF) | tee /tmp/$$-travis-diff.log; grep -c '<' /tmp/$$-travis-diff.log && exit 1; echo "ok"; fi
 
 
 travis_time_start setup_ros
@@ -71,6 +130,15 @@ wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
 lsb_release -a
 sudo apt-get update -q || echo Ignore error of apt-get update
 sudo apt-get install -y --force-yes -q -qq python-rosdep python-wstool python-catkin-tools ros-$ROS_DISTRO-rosbash ros-$ROS_DISTRO-rospack ccache
+# setup catkin-tools option
+if [ ! "$CATKIN_TOOLS_BUILD_OPTIONS" ]; then
+  if [[ "$(pip show catkin-tools | grep '^Version:' | awk '{print $2}')" =~ 0.3.[0-9]+ ]]; then
+    # For catkin-tools==0.3.X, '-iv' option is required to get the stderr output.
+    export CATKIN_TOOLS_BUILD_OPTIONS="-iv --summarize --no-status"
+  else
+    export CATKIN_TOOLS_BUILD_OPTIONS="--summarize --no-status"
+  fi
+fi
 # setup ccache
 sudo ln -s /usr/bin/ccache /usr/local/bin/gcc
 sudo ln -s /usr/bin/ccache /usr/local/bin/g++
@@ -100,9 +168,12 @@ travis_time_end
 travis_time_start setup_catkin
 
 ### before_install: # Use this to prepare the system to install prerequisites or dependencies
-### https://github.com/ros/catkin/pull/705
-[ ! -e /tmp/catkin ] && (cd /tmp/; git clone -q https://github.com/ros/catkin)
-(cd /tmp/catkin; cmake . -DCMAKE_INSTALL_PREFIX=/opt/ros/$ROS_DISTRO/ ; make; sudo make install)
+if [ "$ROS_DISTRO" == "hydro" ]; then
+  [ ! -e /tmp/catkin ] && (cd /tmp/; git clone -q https://github.com/ros/catkin)
+  (cd /tmp/catkin; git checkout 0.6.12; cmake . -DCMAKE_INSTALL_PREFIX=/opt/ros/$ROS_DISTRO/ ; make; sudo make install)
+else
+  sudo apt-get install -y --force-yes -q -qq ros-$ROS_DISTRO-catkin
+fi
 sudo apt-get install -y --force-yes -q -qq ros-$ROS_DISTRO-roslaunch
 ### https://github.com/ros/ros_comm/pull/641
 (cd /opt/ros/$ROS_DISTRO/lib/python2.7/dist-packages; wget --no-check-certificate https://patch-diff.githubusercontent.com/raw/ros/ros_comm/pull/641.diff -O /tmp/641.diff; [ "$ROS_DISTRO" == "hydro" ] && sed -i s@items@iteritems@ /tmp/641.diff ; sudo patch -p4 < /tmp/641.diff)
@@ -122,21 +193,24 @@ travis_time_start setup_rosws
 ### install: # Use this to install any prerequisites or dependencies necessary to run your build
 # Create workspace
 mkdir -p ~/ros/ws_$REPOSITORY_NAME/src
+cd ~/ros/ws_$REPOSITORY_NAME
+catkin init
+catkin config $CATKIN_TOOLS_CONFIG_OPTIONS
 cd ~/ros/ws_$REPOSITORY_NAME/src
 if [ "$USE_DEB" == false ]; then
-    $ROSWS init .
+    wstool init .
     if [ -e $CI_SOURCE_PATH/.travis.rosinstall ]; then
         # install (maybe unreleased version) dependencies from source
-        $ROSWS merge file://$CI_SOURCE_PATH/.travis.rosinstall
+        wstool merge file://$CI_SOURCE_PATH/.travis.rosinstall
     fi
     if [ -e $CI_SOURCE_PATH/.travis.rosinstall.$ROS_DISTRO ]; then
         # install (maybe unreleased version) dependencies from source for specific ros version
-        $ROSWS merge file://$CI_SOURCE_PATH/.travis.rosinstall.$ROS_DISTRO
+        wstool merge file://$CI_SOURCE_PATH/.travis.rosinstall.$ROS_DISTRO
     fi
-    $ROSWS update
+    wstool update
 fi
 ln -s $CI_SOURCE_PATH . # Link the repo we are testing to the new workspace
-if [ "$USE_DEB" == source -a -e $REPOSITORY_NAME/setup_upstream.sh ]; then $ROSWS init .; $REPOSITORY_NAME/setup_upstream.sh -w ~/ros/ws_$REPOSITORY_NAME ; $ROSWS update; fi
+if [ "$USE_DEB" == source -a -e $REPOSITORY_NAME/setup_upstream.sh ]; then wstool init .; $REPOSITORY_NAME/setup_upstream.sh -w ~/ros/ws_$REPOSITORY_NAME ; wstool update; fi
 # disable hrpsys/doc generation
 find . -ipath "*/hrpsys/CMakeLists.txt" -exec sed -i s'@if(ENABLE_DOXYGEN)@if(0)@' {} \;
 # disable metapackage
@@ -173,8 +247,8 @@ fi
 
 travis_time_end
 
-$ROSWS --version
-$ROSWS info -t .
+wstool --version
+wstool info -t .
 cd ../
 
 travis_time_start catkin_build
@@ -184,10 +258,12 @@ source /opt/ros/$ROS_DISTRO/setup.bash > /tmp/$$.x 2>&1; grep export\ [^_] /tmp/
 # for catkin
 if [ "${TARGET_PKGS// }" == "" ]; then export TARGET_PKGS=`catkin_topological_order ${CI_SOURCE_PATH} --only-names`; fi
 if [ "${TEST_PKGS// }" == "" ]; then export TEST_PKGS=$( [ "${BUILD_PKGS// }" == "" ] && echo "$TARGET_PKGS" || echo "$BUILD_PKGS"); fi
-if [ "$BUILDER" == catkin ]; then
-  set -o pipefail  # this is necessary to pipe fail status on grepping
-  catkin build -i --summarize  --no-status $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS | grep -v -e Symlinking -e Linked
-  set +o pipefail
+if [ -z $TRAVIS_JOB_ID ]; then
+  # on Jenkins
+  catkin build $CATKIN_TOOLS_BUILD_OPTIONS $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS --
+else
+  # on Travis, the command must outputs log within 10 min to avoid failures, so the `travis_wait` is necessary.
+  travis_wait 60 catkin build $CATKIN_TOOLS_BUILD_OPTIONS $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS --
 fi
 
 travis_time_end
@@ -200,13 +276,15 @@ if [ "$ROS_DISTRO" == "hydro" ]; then
     (cd /opt/ros/$ROS_DISTRO/share; wget --no-check-certificate https://patch-diff.githubusercontent.com/raw/ros/ros_comm/pull/611.diff -O - | sed s@.cmake.em@.cmake@ | sed 's@/${PROJECT_NAME}@@' | sed 's@ DEPENDENCIES ${_rostest_DEPENDENCIES})@)@' | sudo patch -f -p2 || echo "ok")
 fi
 
-if [ "$BUILDER" == catkin ]; then
-    source devel/setup.bash > /tmp/$$.x 2>&1; grep export\ [^_] /tmp/$$.x ; rospack profile # force to update ROS_PACKAGE_PATH for rostest
-    set -o pipefail  # this is necessary to pipe fail status on grepping
-    catkin run_tests -i --no-deps --no-status $TEST_PKGS $CATKIN_PARALLEL_TEST_JOBS --make-args $ROS_PARALLEL_TEST_JOBS -- | grep -v -e Symlinking -e Linked -e :[^\s]*install[^\s]*\]
-    set +o pipefail
-    catkin_test_results build || error
+source devel/setup.bash > /tmp/$$.x 2>&1; grep export\ [^_] /tmp/$$.x ; rospack profile # force to update ROS_PACKAGE_PATH for rostest
+if [ -z $TRAVIS_JOB_ID ]; then
+  # on Jenkins
+  catkin run_tests -i --no-deps --no-status $TEST_PKGS $CATKIN_PARALLEL_TEST_JOBS --make-args $ROS_PARALLEL_TEST_JOBS --
+else
+  # on Travis
+  travis_wait 60 catkin run_tests -i --no-deps --no-status $TEST_PKGS $CATKIN_PARALLEL_TEST_JOBS --make-args $ROS_PARALLEL_TEST_JOBS --
 fi
+catkin_test_results --verbose --all build || error
 
 travis_time_end
 
@@ -214,38 +292,38 @@ if [ "$NOT_TEST_INSTALL" != "true" ]; then
 
     travis_time_start catkin_install_build
 
-    if [ "$BUILDER" == catkin ]; then
-        catkin clean --yes || catkin clean -a # 0.3.1 uses -a, 0.4.0 uses --yes
-        catkin config --install
-        set -o pipefail  # this is necessary to pipe fail status on grepping
-        catkin build --summarize --no-status $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS | grep -v -e Symlinking -e Linked -e :[^\s]*install[^\s]*\]
-        set +o pipefail
-        source install/setup.bash > /tmp/$$.x 2>&1; grep export\ [^_] /tmp/$$.x
-        rospack profile
-        rospack plugins --attrib=plugin nodelet || echo "ok"
+    catkin clean --yes || catkin clean -a # 0.3.1 uses -a, 0.4.0 uses --yes
+    catkin config --install $CATKIN_TOOLS_CONFIG_OPTIONS
+    if [ -z $TRAVIS_JOB_ID ]; then
+      # on Jenkins
+      catkin build $CATKIN_TOOLS_BUILD_OPTIONS $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS --
+    else
+      # on Travis
+      travis_wait 60 catkin build $CATKIN_TOOLS_BUILD_OPTIONS $BUILD_PKGS $CATKIN_PARALLEL_JOBS --make-args $ROS_PARALLEL_JOBS --
     fi
+    source install/setup.bash > /tmp/$$.x 2>&1; grep export\ [^_] /tmp/$$.x
+    rospack profile
+    rospack plugins --attrib=plugin nodelet || echo "ok"
 
     travis_time_end
     travis_time_start catkin_install_run_tests
 
     export EXIT_STATUS=0
-    if [ "$BUILDER" == catkin ]; then
-      for pkg in $TEST_PKGS; do
-        echo "[$pkg] Started testing..."
-        rostest_files=$(find install/share/$pkg -iname '*.test')
-        echo "[$pkg] Found $(echo $rostest_files | wc -w) tests."
-        for test_file in $rostest_files; do
-          echo "[$pkg] Testing $test_file"
-          rostest $test_file || export EXIT_STATUS=$?
-          if [ $? != 0 ]; then
-            echo -e "[$pkg] Testing again the failed test: $test_file.\e[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
-            rostest --text $test_file
-            echo -e "[$pkg] Testing again the failed test: $test_file.\e[31m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-          fi
-        done
+    for pkg in $TEST_PKGS; do
+      echo "[$pkg] Started testing..."
+      rostest_files=$(test ! -d install/share/$pkg  || find install/share/$pkg -iname '*.test')
+      echo "[$pkg] Found $(echo $rostest_files | wc -w) tests."
+      for test_file in $rostest_files; do
+        echo "[$pkg] Testing $test_file"
+        rostest $test_file || export EXIT_STATUS=$?
+        if [ $? != 0 ]; then
+          echo -e "[$pkg] Testing again the failed test: $test_file.\e[31m>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\e[0m"
+          rostest --text $test_file
+          echo -e "[$pkg] Testing again the failed test: $test_file.\e[31m<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+        fi
       done
-      [ $EXIT_STATUS -eq 0 ] || error  # unless all tests pass, raise error
-    fi
+    done
+    [ $EXIT_STATUS -eq 0 ] || error  # unless all tests pass, raise error
 
     travis_time_end
 
@@ -257,8 +335,8 @@ travis_time_start after_script
 PATH=/usr/local/bin:$PATH  # for installed catkin_test_results
 PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHONPATH
 if [ "${ROS_LOG_DIR// }" == "" ]; then export ROS_LOG_DIR=~/.ros/test_results; fi # http://wiki.ros.org/ROS/EnvironmentVariables#ROS_LOG_DIR
-if [ "$BUILDER" == catkin -a -e $ROS_LOG_DIR ]; then catkin_test_results --verbose --all $ROS_LOG_DIR || error; fi
-if [ "$BUILDER" == catkin -a -e ~/ros/ws_$REPOSITORY_NAME/build/ ]; then catkin_test_results --verbose --all ~/ros/ws_$REPOSITORY_NAME/build/ || error; fi
-if [ "$BUILDER" == catkin -a -e ~/.ros/test_results/ ]; then catkin_test_results --verbose --all ~/.ros/test_results/ || error; fi
+if [ -e $ROS_LOG_DIR ]; then catkin_test_results --verbose --all $ROS_LOG_DIR || error; fi
+if [ -e ~/ros/ws_$REPOSITORY_NAME/build/ ]; then catkin_test_results --verbose --all ~/ros/ws_$REPOSITORY_NAME/build/ || error; fi
+if [ -e ~/.ros/test_results/ ]; then catkin_test_results --verbose --all ~/.ros/test_results/ || error; fi
 
 travis_time_end
